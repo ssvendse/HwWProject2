8d10d25368ed3f148e8afd63f29f8e202d3b9de6
Skyler Svendsen <skylersvendsen@gmail.com>
2017-12-17 19:51:21 -0600

    first commit

--- HwWProject2 WatchKit Extension/InterfaceController.swift
+++ HwWProject2 WatchKit Extension/InterfaceController.swift
@@ -12,10 +12,90 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var question: WKInterfaceImage!
+    @IBOutlet var answers: WKInterfaceGroup!
+
+    @IBOutlet var rock: WKInterfaceButton!
+    @IBOutlet var paper: WKInterfaceButton!
+    @IBOutlet var scissors: WKInterfaceButton!
+    
+    @IBOutlet var levelCounter: WKInterfaceLabel!
+    @IBOutlet var timer: WKInterfaceTimer!
+    
+    @IBOutlet var result: WKInterfaceLabel!
+    
+    var allMoves = ["rock", "paper", "scissors"]
+    var shouldWin = true
+    var level = 1
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        // Configure interface objects here.
+        rock.setBackgroundImage(UIImage(named: "rock"))
+        paper.setBackgroundImage(UIImage(named: "paper"))
+        scissors.setBackgroundImage(UIImage(named: "scissors"))
+
+        timer.start()
+        newLevel()
+    }
+    
+    func newLevel() {
+        if level == 21 {
+            result.setHidden(false)
+            question.setHidden(true)
+            answers.setHidden(true)
+            timer.stop()
+            return
+        }
+        
+        levelCounter.setText("\(level)/20")
+        
+        if  arc4random_uniform(2) == 0 {
+            setTitle("Win!")
+            shouldWin = true
+        } else {
+            setTitle("Lose!")
+            shouldWin = false
+        }
+        
+        allMoves.shuffle()
+        question.setImage(UIImage(named: allMoves[0]))
+    }
+    
+    func check(for answer: String) {
+        if allMoves[0] == answer {
+            level += 1
+            newLevel()
+        } else {
+            level -= 1
+            if level < 1 { level = 1 }
+            newLevel()
+        }
+    }
+    
+    @IBAction func rockTapped() {
+        if shouldWin {
+            check(for: "scissors")
+        } else {
+            check(for: "paper")
+        }
+    }
+
+    @IBAction func paperTapped() {
+        if shouldWin {
+            check(for: "rock")
+        } else {
+            check(for: "scissors")
+        }
+        
+    }
+    
+    @IBAction func scissorsTapped() {
+        if shouldWin {
+            check(for: "paper")
+        } else {
+            check(for: "rock")
+        }
     }
     
     override func willActivate() {
